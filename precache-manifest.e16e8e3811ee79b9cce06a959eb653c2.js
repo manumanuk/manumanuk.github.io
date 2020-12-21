02b18d5492b30d248d263948052b4f8fb698cd69
Aryan Kalia <kalia.aryan2013@gmail.com>
2020-12-20 19:19:40 -0500

    Updates

---
+++ precache-manifest.e16e8e3811ee79b9cce06a959eb653c2.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db3e1ed2830da2601fbacfbf1b7c350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd85ad546cc98c073480",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "519c177179b1f583fba6",
+    "url": "/static/css/main.426f7e7b.chunk.css"
+  },
+  {
+    "revision": "cd85ad546cc98c073480",
+    "url": "/static/js/2.e67f8d34.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/static/js/2.e67f8d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "519c177179b1f583fba6",
+    "url": "/static/js/main.1a6a2fbe.chunk.js"
+  },
+  {
+    "revision": "7c0cf5f8cb2fb73ad3a9",
+    "url": "/static/js/runtime-main.2f3bfa2a.js"
+  },
+  {
+    "revision": "d1e16ef449a359994b286a0b0e322272",
+    "url": "/static/media/arduino_bluetooth.d1e16ef4.jpg"
+  },
+  {
+    "revision": "d33e419b34f1cf6d1ffc7f2da182bd86",
+    "url": "/static/media/code.d33e419b.jpg"
+  },
+  {
+    "revision": "a4fe255447334adb85e8dcf0644b468b",
+    "url": "/static/media/coquito_details.a4fe2554.png"
+  },
+  {
+    "revision": "0e589ea0e95b0f1a247b721c5a37c7f9",
+    "url": "/static/media/coquito_home.0e589ea0.png"
+  },
+  {
+    "revision": "0fd1d568703ef0bf9473b1038fefa832",
+    "url": "/static/media/drone_flying.0fd1d568.png"
+  },
+  {
+    "revision": "537de716529ea6a876261dc74263ecd8",
+    "url": "/static/media/drone_hold.537de716.jpeg"
+  },
+  {
+    "revision": "f9e5f6c2f0e3b163c2ff75e714572788",
+    "url": "/static/media/fitbit_app.f9e5f6c2.jpg"
+  },
+  {
+    "revision": "eb038977db4d694f38b2b2a70133333e",
+    "url": "/static/media/github_logo.eb038977.svg"
+  },
+  {
+    "revision": "50d2708b833f501fc7fb4c73c90c97fc",
+    "url": "/static/media/homebkg.50d2708b.jpg"
+  },
+  {
+    "revision": "bd9c1249f15c5a4d65f667b8b255b240",
+    "url": "/static/media/ics_3u0.bd9c1249.png"
+  },
+  {
+    "revision": "1fed9c6506cd3fad9f2b929f4377d45f",
+    "url": "/static/media/line_follower_1.1fed9c65.jpg"
+  },
+  {
+    "revision": "e0cdca9c662ddb55a6f5c0927f9be040",
+    "url": "/static/media/line_follower_2.e0cdca9c.jpg"
+  },
+  {
+    "revision": "92671aae50d22fe5949d79c42c8cf491",
+    "url": "/static/media/line_follower_3.92671aae.jpg"
+  },
+  {
+    "revision": "7259350694a5e3fcb203059af2c5994e",
+    "url": "/static/media/livelaunch.72593506.png"
+  },
+  {
+    "revision": "8ca59e2a2871783e28fa4a53823d1714",
+    "url": "/static/media/mail_icon_dark.8ca59e2a.jpg"
+  },
+  {
+    "revision": "82b8b5057b5011240ac62305e2ed59b0",
+    "url": "/static/media/mail_icon_light.82b8b505.jpg"
+  },
+  {
+    "revision": "b6f8703a98b03e45aedc6c4a0777fa44",
+    "url": "/static/media/moon.b6f8703a.jpg"
+  },
+  {
+    "revision": "a1c9f6ffba8f36f1f088249fb5ae3fb1",
+    "url": "/static/media/ni_labview.a1c9f6ff.jpg"
+  },
+  {
+    "revision": "0d3e3263eaf1358bddbfe75479bb59ee",
+    "url": "/static/media/ni_mydaq.0d3e3263.jpg"
+  },
+  {
+    "revision": "163574f03082ac3c9091b0caba8c7289",
+    "url": "/static/media/redboard.163574f0.JPG"
+  },
+  {
+    "revision": "b491ebfae04571e32a43c8d10a52ac4b",
+    "url": "/static/media/redboard_dark.b491ebfa.jpg"
+  },
+  {
+    "revision": "37066fed88749e1aeb7857a934e4b3a0",
+    "url": "/static/media/sonify_convert.37066fed.png"
+  },
+  {
+    "revision": "af605ad8a33fbc4697eaf290f82741ce",
+    "url": "/static/media/sonify_home.af605ad8.png"
+  },
+  {
+    "revision": "7b993ba6b7974e282921788eab85195d",
+    "url": "/static/media/tello_drone.7b993ba6.jpg"
+  },
+  {
+    "revision": "0fd1d568703ef0bf9473b1038fefa832",
+    "url": "/static/media/test-image.0fd1d568.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd44f5c6a9f7735011c22d9241b6ad2c.js"
+  "/precache-manifest.e16e8e3811ee79b9cce06a959eb653c2.js"
 );
 
 self.addEventListener('message', (event) => {
